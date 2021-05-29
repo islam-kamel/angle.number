4f670e0707a4d21d41d585de074ee1f97f804c49
islam kamel <islam-kamel@users.noreply.github.com>
2021-05-29 15:07:27 +0200

    add message audio

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 // DOM selectors
 const stars = document.getElementById('stars');
 const starsCtx = stars.getContext('2d');
-const slider = document.querySelector(".slider input");
-const output = document.querySelector("#speed");
 
 // global variables
-let screen, starsElements, starsParams = { speed: 0.5, number: 300, extinction: 4 };
+let screen, starsElements, starsParams = { speed: 0.5, number: 300, extinction: 4 }, audio;
 
 // run stars
 setupStars();
@@ -54,7 +52,7 @@ function setupStars() {
     };
     window.cancelAnimationFrame(updateStars);
     stars.width = screen.w;
-    stars.height = screen.h;
+    stars.height = screen.h - document.getElementsByClassName('navbar')[0].offsetHeight;
     starsElements = [];
     for (let i = 0; i < starsParams.number; i++) {
         starsElements[i] = new Star();
@@ -72,7 +70,101 @@ function updateStars() {
     window.requestAnimationFrame(updateStars);
 }
 
-const number = document.getElementById("number").value;
-if (number == "9"){
-    document.getElementById("#background_music").autoplay
+
+function get_number()
+{
+    const number = document.getElementById("number").value;
+    toggle_Audio()
+    if (number >= 0)
+    {
+        if (get_angle(number) > "9")
+        {
+            get_angle(number)
+        }
+    }
+
 }
+
+function get_angle(number)
+{
+    if (number > 10)
+    {
+        let temp = sum(number),
+        angle_number = 0;
+
+        if (temp > 10){
+            angle_number = sum(temp)
+        }
+        else
+        {
+           angle_number = sum(temp)
+        }
+    }
+    else if (number <= 10)
+    {
+        switch (number)
+        {
+            case "0":
+                play_message(number)
+                break;
+            case "1":
+                play_message(number)
+                break;
+            case "2":
+                play_message(number)
+                break;
+            case "3":
+                play_message(number)
+                break;
+            case "4":
+                play_message(number)
+                break;
+            case "5":
+                play_message(number)
+                break;
+            case "6":
+                play_message(number)
+                break;
+            case "7":
+                play_message(number)
+                break;
+            case "8":
+                play_message(number)
+                break;
+            case "9":
+                play_message(number)
+                break;
+            case "10":
+                play_message(number)
+                break;
+        }
+    }
+
+}
+
+function sum(number)
+{
+    let value = number,
+    sum = 0;
+    while (value) {
+        sum += value % 10;
+        value = Math.floor(value / 10);
+    }
+    return sum
+}
+
+function play_message(number)
+{
+    audio = new Audio(`audio/${number}.mp3`);
+    audio.play()
+}
+
+function toggle_Audio()
+{
+    if (audio !== undefined)
+    {
+        audio.pause()
+    }
+}
+const margin = screen.h / 2 -  document.getElementsByClassName("card")[0].offsetHeight
+document.getElementsByClassName("card")[0].setAttribute("style", `margin-top: ${margin}px !important`)
